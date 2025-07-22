365c5f9f07d1f3f39c20d3ead1d419dc3c31bb76
Amresh <cote4362@gmail.com>
2025-07-22 23:55:49 +0530

    feat: enhance transaction management with validation, retrieval, update, and deletion functionality

--- backend/src/controllers/transaction.controller.js
+++ backend/src/controllers/transaction.controller.js
@@ -14,7 +14,13 @@ const addTransacion = async (req, res, next) => {
     }
 
     try {
-        if ([description, amount, type, transactionDate, category].some((field) => field.trim() === "")) {
+        if (
+            !description?.trim() ||
+            !amount ||
+            !type?.trim() ||
+            !transactionDate ||
+            !category?.trim()
+        ) {
             throw new ApiError(400, "All fields are required!")
         }
 
@@ -27,13 +33,13 @@ const addTransacion = async (req, res, next) => {
             category
         })
 
-        const saveTransaction = await newTransaction.save()
+        await newTransaction.save()
 
-        const createdTransaction = await UserTransaction.findById(newTransaction._id)
+        // const createdTransaction = await newTransaction;
 
-        if (!createdTransaction) {
-            throw new ApiError(500, "Transaction creation failed")
-        }
+        // if (!createdTransaction) {
+        //     throw new ApiError(500, "Transaction creation failed")
+        // }
 
         return res.status(201).json(
             new ApiResponse(200, createdTransaction, "Transaction created successfully")
@@ -42,3 +48,109 @@ const addTransacion = async (req, res, next) => {
         throw new ApiError(500, error?.message || "Transaction creation failed")
     }
 }
+
+// Retrieves a list of all financial transactions belonging to the authenticated user. 
+const getTransactions = async (req, res, _) => {
+    
+    try {
+        const query  = {userId: req.user._id}
+
+        // Apply filters based on query parameters
+        if (req.query.type)  {
+            query.type = req.query.type
+        }
+
+        if (req.query.category) {
+            query.category = req.query.category
+        }
+
+        if (req.query.startDate && req.query.endDate) {
+            query.transactionDate = {
+                $gte: new Date(req.query.startDate),
+                $lte: new Date(req.query.endDate)
+            }
+        }
+
+        const { sortBy, sortOrder } = req.query
+
+        let sortOptions = { transactionDate: -1 } //  If no sort is specified by the user, sort by transactionDate in descending order (latest first)
+
+        if (sortBy && sortOrder) {
+            const order = sortOrder === "desc" ? -1 : 1;
+            sortOptions = { [sortBy]: order}
+        }
+
+        const transactions = await UserTransaction
+        .find(query)
+        .sort(sortOptions)
+
+        res.status(200).json(
+            new ApiResponse(200, transactions, "Transactions retrieved successfully")
+        )
+    } catch (error) {
+        res.status(500).json({
+            error: error.message || "Failed to retrieve transactions"
+        })
+    }
+
+}
+
+const updateTransaction = async (req, res, _) => {
+
+    try {
+        const updateTransaction = await UserTransaction.findOneAndUpdate(
+            {
+                // The transaction ID — passed in the URL like /transactions/:id. Used to identify which transaction to update.
+                _id: req.params.id,
+                userId: req.user._id
+            },
+            req.body,
+            { new: true, runValidators: true }
+            // Returns updated doc instead of old one, Ensures amount, type, etc., are valid
+        );
+
+        if (!updateTransaction){
+            return res.status(404).json({
+                message: "Transaction not found or you are not authorized to update it"
+            })
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200, updateTransaction, "Transaction updated successfully")
+        )
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message || "Failed to update transaction"
+        })
+    }
+};
+
+const deleteTransaction = async (req, res, _) => {
+    
+    try {
+        const deleteTransaction = await UserTransaction.findOneAndDelete(
+            {
+                _id: req.params.id,
+                userId: req.user._id
+            },
+        );
+
+        if (!deleteTransaction){
+            return res.status(404).json({
+                message: "Transaction not found or you are not authorized to delete it"
+            })
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200, deleteTransaction, "Transaction deleted successfully")
+        )
+
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message || "Failed to delete transaction"
+        })
+    }
+
+}
+
+export { addTransacion, getTransactions, updateTransaction, deleteTransaction}
